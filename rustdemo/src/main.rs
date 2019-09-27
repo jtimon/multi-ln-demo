2a092a925f508ae670c6f2b77ed23307ef3bd5fb
Jorge Timón <jtimon@jtimon.cc>
2019-09-28 01:19:32 +0200

    rustdemo: demo: We really only need 1 bitcoind per chain

--- rustdemo/src/main.rs
+++ rustdemo/src/main.rs
@@ -9,19 +9,7 @@ struct RpcCaller {
 #[derive(Debug)]
 struct ChainConfig {
     port_decimal: u32,
-    main_user: String,
     users: Vec<String>,
-}
-
-impl ChainConfig {
-
-    pub fn get_p2p_decimal(&self, user_name: &str) -> &str {
-        // TODO This only scales to 2 nodes per chain
-        if &self.main_user == user_name {
-            return "5";
-        }
-        "6"
-    }
 }
 
 fn main() {
@@ -31,12 +19,10 @@ fn main() {
 
     chains.insert("chain_1", ChainConfig {
         port_decimal: 4,
-        main_user: String::from("alice"),
         users: vec![String::from("alice"), String::from("bob")],
     });
     chains.insert("chain_2", ChainConfig {
         port_decimal: 5,
-        main_user: String::from("bob"),
         users: vec![String::from("bob"), String::from("carol")],
     });
 
@@ -47,8 +33,7 @@ fn main() {
 
         let mut bitcoinds_for_chain = std::collections::HashMap::new();
         for user in chain_config.users.iter() {
-            let p2p_decimal = chain_config.get_p2p_decimal(&user);
-            let rpc_port = format!("18{}{}5", p2p_decimal, chain_config.port_decimal);
+            let rpc_port = format!("185{}5", chain_config.port_decimal);
             bitcoinds_for_chain.insert(user, RpcCaller {
                 addr: format!("0.0.0.0:{}", rpc_port),
                 user: format!("user{}", rpc_port),
