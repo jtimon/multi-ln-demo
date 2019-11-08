003a9a623be01983e1fa745bc0188f3d19ba9c95
Jorge Timón <jtimon@jtimon.cc>
2019-11-09 00:58:09 +0100

    demo: add id and petname to demo's chain.py

--- multiln/chains.py
+++ multiln/chains.py
@@ -1,31 +1,43 @@
 
 CHAINS = {
     'regtest': {
+        'id': '0f9188f13cb7b2c71f2a335e3a4fc328bf5beb436012afca590b1a11466e2206',
+        'petname': 'regtest',
         'port_decimal': 1,
         'users': ['alice', 'bob'],
     },
 
     'chain_1': {
+        'id': '58ebd25d25b128530d4d462c65a7e679b7e053e6f25ffb8ac63bc68832fda201',
+        'petname': 'chain_1',
         'port_decimal': 4,
         'users': ['alice', 'bob'],
     },
 
     'chain_2': {
+        'id': 'e07d79a4f8f1525814e421eb71aa9527fe8a25091fe1b9c5c312939c887aadc7',
+        'petname': 'chain_2',
         'port_decimal': 5,
         'users': ['bob', 'carol'],
     },
 
     'chain_3': {
+        'id': 'de650213b96a541df3bd9ee530cc3da4e9424d3617f95e6b2a0d5452e23412b9',
+        'petname': 'chain_3',
         'port_decimal': 6,
         'users': ['carol', 'david'],
     },
 
     'chain_4': {
+        'id': '075e818d62bbe049a715856344987294ea2b4ff636b857911966e7fc9fee637c',
+        'petname': 'chain_4',
         'port_decimal': 7,
         'users': ['david', 'ezra'],
     },
 
     'chain_5': {
+        'id': '54a435af6a093f145769b138c20d5f72b35395e7057a89d36a0b8e954031b04c',
+        'petname': 'chain_5',
         'port_decimal': 8,
         'users': ['ezra', 'fiona'],
     },
