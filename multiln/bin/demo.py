ff2357c77e028fbff8e3c7d7d5ed16e02731188b
Jorge Timón <jtimon@jtimon.cc>
2019-11-15 00:24:43 +0100

    demo: stop telling the story if there's an error instead of a src_invoice

--- multiln/bin/demo.py
+++ multiln/bin/demo.py
@@ -98,6 +98,7 @@ def demo_2_chains_fail(lightningd_map):
     }).json()
     print("...but since %s can't pay to chain %s, pays the following invoice to %s gateway inc in chain %s instead..." % (user_name_a, chain_name_b, user_name_gateway, chain_name_a))
     print('src_invoice:', src_invoice)
+    if 'error' in src_invoice: return
 
     try:
         src_payment_result = lightningd_map[chain_name_a][user_name_a].pay(src_invoice['bolt11'])
