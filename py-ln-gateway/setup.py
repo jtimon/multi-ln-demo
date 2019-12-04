85b22099a1d8a3b944bbba63483ec77a6057bc2f
Jorge Timón <jtimon@jtimon.cc>
2019-12-04 20:06:00 +0100

    gateway: typo

--- py-ln-gateway/setup.py
+++ py-ln-gateway/setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name = "py-ln-gateway",
     version = "0.0.1",
-    description='A gateway server to facillitate lightning payments across several different chains',
+    description='A gateway server to facilitate lightning payments across several different chains',
     author='Jorge Timón',
     packages=['py_ln_gateway'],
 )
