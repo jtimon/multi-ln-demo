95c3d9c964615e09e593ef6091ab0deb16ed8cd4
Jorge Timón <jtimon@jtimon.cc>
2019-12-05 21:54:26 +0100

    gateway: sqlalchemy: Remove SQLALCHEMY_TRACK_MODIFICATIONS warning

--- py-ln-gateway/py_ln_gateway/app.py
+++ py-ln-gateway/py_ln_gateway/app.py
@@ -6,5 +6,6 @@ from py_ln_gateway.gateway_blueprint import gateway_blueprint
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////wd/py-ln-gateway/py_ln_gateway/gatewaydb.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 app.register_blueprint(gateway_blueprint)
