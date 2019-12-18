852c812b48f43e8068c81621ce318cbfe356c9cb
Jorge Timón <jtimon@jtimon.cc>
2019-12-18 04:05:59 +0100

    Change from 5-chains to 3-chains
    Paying from user Alice to David should be enough, no need for fiona

--- multiln/chains.py
+++ multiln/chains.py
@@ -20,18 +20,4 @@ CHAINS = {
         'port_decimal': 6,
         'users': ['carol', 'david'],
     },
-
-    'chain_4': {
-        'id': '075e818d62bbe049a715856344987294ea2b4ff636b857911966e7fc9fee637c',
-        'petname': 'chain_4',
-        'port_decimal': 7,
-        'users': ['david', 'ezra'],
-    },
-
-    'chain_5': {
-        'id': '54a435af6a093f145769b138c20d5f72b35395e7057a89d36a0b8e954031b04c',
-        'petname': 'chain_5',
-        'port_decimal': 8,
-        'users': ['ezra', 'fiona'],
-    },
 }
