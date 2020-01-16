664c0cef29b463d96087a091f8c05a15e7acd67b
Jorge Timón <jtimon@jtimon.cc>
2020-01-16 04:28:18 +0100

    Docker: Share entrypoint-bitcoind.sh

--- docker/entrypoint-bitcoind.sh
+++ docker/entrypoint-bitcoind.sh
@@ -10,4 +10,4 @@ mkdir -p /wd/daemon-data/regtest_bitcoind
 mkdir -p /wd/daemon-data/chain_2_bitcoind
 mkdir -p /wd/daemon-data/chain_3_bitcoind
 
-honcho start -f /wd/docker/3-chains/bitcoind.Procfile
+honcho start -f ${BITCOIND_PROCFILE}
