ee90da5270ce8ab0d0d17a3eb117b09efb101e26
Jorge Timón <jtimon@jtimon.cc>
2020-01-17 20:57:29 +0100

    Docker: Use named volumes for bitcoind containers

--- docker/entrypoint-bitcoind.sh
+++ docker/entrypoint-bitcoind.sh
@@ -3,11 +3,4 @@
 # Causes the shell to exit if any subcommand or pipeline returns a non-zero status
 set -e
 
-mkdir -p /wd/daemon-data
-chown -R $user:$user /wd/daemon-data
-
-mkdir -p /wd/daemon-data/regtest_bitcoind
-mkdir -p /wd/daemon-data/chain_2_bitcoind
-mkdir -p /wd/daemon-data/chain_3_bitcoind
-
 ${BITCOIND_CALL}
