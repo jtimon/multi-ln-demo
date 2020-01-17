fc3c8bb34d2d751bc84c89527c56357fdc646685
Jorge Timón <jtimon@jtimon.cc>
2020-01-17 20:28:59 +0100

    Docker: Separate clightning volumes

--- docker/entrypoint-lightningd.sh
+++ docker/entrypoint-lightningd.sh
@@ -3,11 +3,4 @@
 # Causes the shell to exit if any subcommand or pipeline returns a non-zero status
 set -e
 
-mkdir -p /wd/daemon-data/alice_regtest
-mkdir -p /wd/daemon-data/bob_regtest
-mkdir -p /wd/daemon-data/bob_chain_2
-mkdir -p /wd/daemon-data/carol_chain_2
-mkdir -p /wd/daemon-data/carol_chain_3
-mkdir -p /wd/daemon-data/david_chain_3
-
 honcho start -f ${LIGHTNINGD_PROCFILE}
