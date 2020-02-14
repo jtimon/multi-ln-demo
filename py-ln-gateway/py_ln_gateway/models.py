b2387f903d8fbcc022bbfdb8eddf79bc06e07313
Jorge Timón <jtimon@jtimon.cc>
2020-02-14 05:10:21 +0100

    fixup better

--- py-ln-gateway/py_ln_gateway/models.py
+++ py-ln-gateway/py_ln_gateway/models.py
@@ -29,7 +29,7 @@ class PendingRequest(Base):
     src_expires_at = Column(DateTime(), nullable=False)
     src_amount = Column(Integer(), nullable=False)
 
-    dest_payment_hash = Column(String(FIELD_32B_AS_HEX_STR), nullable=False)
+    dest_payment_hash = Column(String(FIELD_32B_AS_HEX_STR))
     dest_chain = Column(String(64), nullable=False)
     dest_bolt11 = Column(String(MAX_BOLT11), nullable=False)
     dest_expires_at = Column(DateTime())
