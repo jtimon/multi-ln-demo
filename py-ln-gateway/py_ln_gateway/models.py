d2e637cb8c16e3d6a9679260ec738ac32b5580cc
Jorge Timón <jtimon@jtimon.cc>
2020-02-25 23:15:14 +0100

    gateway: db: optimization: Store 32 bytes instead of 64 hex chars

--- py-ln-gateway/py_ln_gateway/models.py
+++ py-ln-gateway/py_ln_gateway/models.py
@@ -1,5 +1,5 @@
 
-from sqlalchemy import Column, DateTime, Integer, Numeric, String
+from sqlalchemy import Column, DateTime, Integer, Numeric, String, LargeBinary, TypeDecorator
 from py_ln_gateway.db import Base
 
 # There is no absolute limit for bolt11. There are practical limits based on QR code sizes.
@@ -8,36 +8,46 @@ from py_ln_gateway.db import Base
 MAX_BOLT11 = 2048
 MAX_URL_LEN = 200
 
-# TODO optimization: use bytes.fromhex() and store 32 bytes instead of 64 char hex string
-# Find out best field for this on sqlalchemy
-FIELD_32B_AS_HEX_STR = 64
+class ByteHexString(TypeDecorator):
+    """Convert a string with hexadecimal digits to bytestring for storage and back."""
+    impl = LargeBinary
+
+    def process_bind_param(self, value, dialect):
+        if not value:
+            return None
+        if not isinstance(value, str):
+            raise TypeError("ByteHexString columns support only str values.")
+        return bytes.fromhex(value)
+
+    def process_result_value(self, value, dialect):
+        return value.hex() if value else None
 
 class Price(Base):
     __tablename__ = 'prices'
 
     # The id is composed by the src_chain_id followed by the dest_chain_id
-    src_dest = Column(String(2 * FIELD_32B_AS_HEX_STR), primary_key=True)
+    src_dest = Column(ByteHexString(2 * 32), primary_key=True)
     price = Column(Numeric(10,4), nullable=False)
 
 class PendingRequest(Base):
     __tablename__ = 'pending_requests'
 
-    src_payment_hash = Column(String(FIELD_32B_AS_HEX_STR), primary_key=True)
+    src_payment_hash = Column(ByteHexString(32), primary_key=True)
 
-    src_chain = Column(String(FIELD_32B_AS_HEX_STR), nullable=False)
+    src_chain = Column(ByteHexString(32), nullable=False)
     src_bolt11 = Column(String(MAX_BOLT11), nullable=False)
     src_expires_at = Column(DateTime(), nullable=False)
     src_amount = Column(Integer(), nullable=False)
 
-    dest_payment_hash = Column(String(FIELD_32B_AS_HEX_STR), nullable=False, unique=True)
-    dest_chain = Column(String(64), nullable=False)
+    dest_payment_hash = Column(ByteHexString(32), nullable=False, unique=True)
+    dest_chain = Column(ByteHexString(32), nullable=False)
     dest_bolt11 = Column(String(MAX_BOLT11), nullable=False)
     dest_expires_at = Column(DateTime(), nullable=False)
     dest_amount = Column(Integer(), nullable=False)
 
-    other_gw_payment_hash = Column(String(FIELD_32B_AS_HEX_STR))
+    other_gw_payment_hash = Column(ByteHexString(32))
     other_gw_url = Column(String(MAX_URL_LEN))
-    other_gw_chain = Column(String(64))
+    other_gw_chain = Column(ByteHexString(32))
     other_gw_bolt11 = Column(String(MAX_BOLT11))
     other_gw_expires_at = Column(DateTime())
     other_gw_amount = Column(Integer())
@@ -45,49 +55,49 @@ class PendingRequest(Base):
 class PaidRequest(Base):
     __tablename__ = 'paid_requests'
 
-    src_payment_hash = Column(String(FIELD_32B_AS_HEX_STR), primary_key=True)
+    src_payment_hash = Column(ByteHexString(32), primary_key=True)
 
-    src_chain = Column(String(FIELD_32B_AS_HEX_STR), nullable=False)
+    src_chain = Column(ByteHexString(32), nullable=False)
     src_bolt11 = Column(String(MAX_BOLT11), nullable=False)
     src_expires_at = Column(DateTime(), nullable=False)
-    src_payment_preimage = Column(String(FIELD_32B_AS_HEX_STR), nullable=False)
+    src_payment_preimage = Column(ByteHexString(32), nullable=False)
 
-    dest_payment_hash = Column(String(FIELD_32B_AS_HEX_STR), nullable=False, unique=True)
-    dest_chain = Column(String(FIELD_32B_AS_HEX_STR), nullable=False)
+    dest_payment_hash = Column(ByteHexString(32), nullable=False, unique=True)
+    dest_chain = Column(ByteHexString(32), nullable=False)
     dest_bolt11 = Column(String(MAX_BOLT11), nullable=False)
     dest_expires_at = Column(DateTime(), nullable=False)
-    dest_payment_preimage = Column(String(FIELD_32B_AS_HEX_STR), nullable=False)
+    dest_payment_preimage = Column(ByteHexString(32), nullable=False)
 
-    other_gw_payment_hash = Column(String(FIELD_32B_AS_HEX_STR))
+    other_gw_payment_hash = Column(ByteHexString(32))
     other_gw_url = Column(String(MAX_URL_LEN))
-    other_gw_chain = Column(String(FIELD_32B_AS_HEX_STR))
+    other_gw_chain = Column(ByteHexString(32))
     other_gw_bolt11 = Column(String(MAX_BOLT11))
     other_gw_expires_at = Column(DateTime())
-    other_gw_payment_preimage = Column(String(FIELD_32B_AS_HEX_STR))
+    other_gw_payment_preimage = Column(ByteHexString(32))
 
 class FailedRequest(Base):
     __tablename__ = 'failed_requests'
 
-    src_payment_hash = Column(String(FIELD_32B_AS_HEX_STR), primary_key=True)
+    src_payment_hash = Column(ByteHexString(32), primary_key=True)
 
     error = Column(String(2500))
-    src_payment_preimage = Column(String(FIELD_32B_AS_HEX_STR))
+    src_payment_preimage = Column(ByteHexString(32))
 
-    src_chain = Column(String(FIELD_32B_AS_HEX_STR), nullable=False)
+    src_chain = Column(ByteHexString(32), nullable=False)
     src_bolt11 = Column(String(MAX_BOLT11), nullable=False)
     src_expires_at = Column(DateTime(), nullable=False)
     src_amount = Column(Integer(), nullable=False)
 
-    dest_payment_hash = Column(String(FIELD_32B_AS_HEX_STR), nullable=False, unique=True)
-    dest_chain = Column(String(FIELD_32B_AS_HEX_STR), nullable=False)
+    dest_payment_hash = Column(ByteHexString(32), nullable=False, unique=True)
+    dest_chain = Column(ByteHexString(32), nullable=False)
     dest_bolt11 = Column(String(MAX_BOLT11), nullable=False)
     dest_expires_at = Column(DateTime(), nullable=False)
     dest_amount = Column(Integer(), nullable=False)
 
-    other_gw_payment_hash = Column(String(FIELD_32B_AS_HEX_STR))
+    other_gw_payment_hash = Column(ByteHexString(32))
     other_gw_url = Column(String(MAX_URL_LEN))
-    other_gw_chain = Column(String(64))
+    other_gw_chain = Column(ByteHexString(32))
     other_gw_bolt11 = Column(String(MAX_BOLT11))
     other_gw_expires_at = Column(DateTime())
     other_gw_amount = Column(Integer())
-    other_gw_payment_preimage = Column(String(FIELD_32B_AS_HEX_STR))
+    other_gw_payment_preimage = Column(ByteHexString(32))
