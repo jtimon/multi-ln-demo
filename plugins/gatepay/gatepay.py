d723feb2d6e58d076473415d4c06e23a9d96a618
Jorge Timón <jtimon@jtimon.cc>
2020-03-09 06:21:11 +0100

    gatepay: DRY: refactor: Introduce has_error()

--- plugins/gatepay/gatepay.py
+++ plugins/gatepay/gatepay.py
@@ -13,6 +13,10 @@ def check_hash_preimage(payment_hash, payment_preimage):
     hashed_result = sha256(bytes.fromhex(payment_preimage)).hexdigest()
     return hashed_result == payment_hash
 
+def has_error(var_name, var_val):
+    plugin.log('GATEPAY: %s (%s)' % (var_name, var_val))
+    return not isinstance(var_val, dict) or 'error' in var_val
+
 @plugin.method("gatepay")
 def gatepay(plugin, bolt11, src_chain_ids, gateway, payment_hash):
     """This is like the pay plugin but with more chances to actually pay.
@@ -43,22 +47,18 @@ def gatepay(plugin, bolt11, src_chain_ids, gateway, payment_hash):
                 'bolt11': bolt11,
                 'src_chain_ids': src_chain_ids,
             }).json()
-            # TODO: DRY: check error function which logs and retunrs true/false
-            plugin.log('GATEPAY: src_invoice (%s)' % src_invoice)
-            if 'error' in src_invoice:
+            if has_error('src_invoice', src_invoice):
                 return {'error': src_invoice['error']}
 
             src_payment_result = lnnode.pay(src_invoice['bolt11'])
-            plugin.log('GATEPAY: src_payment_result (%s)' % src_payment_result)
-            if 'error' in src_payment_result:
+            if has_error('src_payment_result', src_payment_result):
                 return {'error': src_payment_result['error']}
 
             gateway_confirm_payment_result = requests.post(gateway + "/confirm_src_payment", data={
                 'payment_hash': src_payment_result['payment_hash'],
                 'payment_preimage': src_payment_result['payment_preimage'],
             }).json()
-            plugin.log('GATEPAY: gateway_confirm_payment_result (%s)' % gateway_confirm_payment_result)
-            if 'error' in gateway_confirm_payment_result:
+            if has_error('gateway_confirm_payment_result', gateway_confirm_payment_result):
                 return {'error': gateway_confirm_payment_result['error']}
 
             if check_hash_preimage(payment_hash, gateway_confirm_payment_result['payment_preimage']):
